e9f4a2d3c490c2e3309edaa00be42a26143ba264
mark <markg2@att.net>
2014-05-19 18:59:33 -0700

    Added startup servlet.  This will eventually hold singleton Mongo resources (e.g. connection related items)

---
+++ cody/src/main/java/com/bapple/Startup.java
@@ -0,0 +1,29 @@
+package com.bapple;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class Startup
+ */
+public class Startup extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Startup() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+//		System.out.println("Startup servlet invoked successfully.");
+	}
+
+}
