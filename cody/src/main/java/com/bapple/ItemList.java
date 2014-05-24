35842b407728cc7c77a826b44a25c25615b32933
mark <markg2@att.net>
2014-05-24 12:04:02 -0700

    List by user
    List of items for the specified user
    - list created from DB now
    - provide count of items in list

--- cody/src/main/java/com/bapple/ItemList.java
+++ cody/src/main/java/com/bapple/ItemList.java
@@ -1,10 +1,16 @@
 package com.bapple;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.HEAD;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
 
 @Path("/itemlist")
 public class ItemList {
@@ -15,22 +21,40 @@ public class ItemList {
 	 * @return a String in JSON format containing all of the documents to be returned
 	 */
 	@GET
-//	@Path("/{id:[a-f0-9\\-]+}")
-	@Produces(MediaType.TEXT_PLAIN)
-	public String getAsText(@PathParam("id") final String strCollection) {
-		return "{ \"name\" : \"Das Parka\", \"itemId\" : \"84102\", \"manufacturer\" : \"patagonia.com\" }" +
-			"{ \"name\" : \"Snowstorm 20 Backpack - 1220 cu in\", \"itemId\" : \"\", \"manufacturer\" : \"Lowe Alpine\" }" +
-			"{ \"name\" : \"Snowstorm 20 Backpack - 1220 cu in\", \"itemId\" : \"123456\", \"manufacturer\" : \"Lowe Alpine\" }" +
-			"{ \"name\" : \"Attack MX50+10 Pack - 3000 cu in\", \"itemId\" : \"\", \"manufacturer\" : \"Lowe Alpine\" }" +
-			"{ \"name\" : \"Serendipity Jacket\", \"itemId\" : \"\", \"manufacturer\" : \"Cloudveil\" }" +
-			"{ \"name\" : \"Alpine Guide Jacket\", \"itemId\" : \"83940\", \"manufacturer\" : \"patagonia.com\" }" +
-			"{ \"name\" : \"Ascensionist Pack\", \"itemId\" : \"48000\", \"manufacturer\" : \"patagonia.com\" }" +
-			"{ \"name\" : \"Gritty Pack\", \"itemId\" : \"\", \"manufacturer\" : \"patagonia.com\" }" +
-			"{ \"name\" : \"Sprint Action Sport Pack\", \"itemId\" : \"\", \"manufacturer\" : \"Lowe Alpine\" }" +
-			"{ \"name\" : \"Troposphere Jacket\", \"itemId\" : \"84540\", \"manufacturer\" : \"patagonia.com\" }" +
-			"{ \"name\" : \"Mixed Guide Hoody (hybrid hard/softshell)\", \"itemId\" : \"\", \"manufacturer\" : \"patagonia.com\" }" +
-			"{ \"name\" : \"Triolet Jacket\", \"itemId\" : \"\", \"manufacturer\" : \"patagonia.com\" }" +
-			"{ \"name\" : \"Mountain Jacket\", \"itemId\" : \"\", \"manufacturer\" : \"TNF\" }";
+	@Path("/{id:[a-f0-9\\-]+}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getListByUser(@PathParam("id") final String strUser) {
+		DB db = ConnectionManagerFactory.getFactory().getConnection();
+		
+		DBCollection coll = db.getCollection("item");
+		BasicDBObject objToFind = new BasicDBObject("user", strUser);
+		DBCursor cursor = coll.find(objToFind);
+		String strDocs = "[";
+		String strComma = "";
+
+		while (cursor.hasNext()) {
+			strDocs += strComma;
+			strDocs += cursor.next();
+			strComma = ",";
+		}
+		
+		strDocs += "]";
+
+		return strDocs;
 	}
 
+	
+	@GET
+	@Path("/{id:[a-f0-9\\-]+}/count")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getListByUserCount(@PathParam("id") final String strUser) {
+		DB db = ConnectionManagerFactory.getFactory().getConnection();
+		
+		DBCollection coll = db.getCollection("item");
+		BasicDBObject objToFind = new BasicDBObject("user", strUser);
+		int itemCount = coll.find(objToFind).count();
+
+		return "{\"count\":" + Integer.valueOf(itemCount).toString() + "}";
+	}
+	
 }
